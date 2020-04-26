ecc75e6dbcf4c995d2a31c170127562c642df97b
عرفان صابری | Erfan Saberi <55283540+erfansaberi@users.noreply.github.com>
2020-04-26 21:08:57 +0430

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import requests
+import datetime
+from flask import Flask, render_template, request, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def getremainingtime():
+    '''this function return a string that contain the remaining hours and minutes of your fast'''
+    try:
+        if request.args.get('citycode'):
+            citycode = request.args.get('citycode')
+        elif request.form.get('citycode'):
+            citycode = request.form.get('citycode')
+        citycode = int(citycode)
+    except:
+        if 'json' in request.args:
+            return jsonify({'status':'error'})
+        return render_template('home.html',data={'status':'error'})
+    now = datetime.datetime.now().time()
+    try:
+        '''using aviny.com api to get maghreb and sobh azan time'''
+        oghat = requests.get(f'https://prayer.aviny.com/api/prayertimes/{citycode}')
+    except:
+        return 'An error occurred during connecting to "Aviny.com" API.'
+    if oghat.ok:
+        maghreb = oghat.json().get('Maghreb')
+        sobh = oghat.json().get('Imsaak')
+        shahr = oghat.json().get('CityName')
+        if not shahr:
+            return render_template('home.html',data={'status':'cityNotFound'})
+        '''convert maghreb and sobh to datetime type'''
+        maghrebtime = datetime.datetime.strptime(maghreb, '%H:%M:%S').time()
+        sobhtime = datetime.datetime.strptime(sobh, '%H:%M:%S').time()
+        if now > maghrebtime:
+            return render_template('home.html',data={'status':'eftar','shahr':shahr})
+        if now < sobhtime:
+            return render_template('home.html',data={'status':'notstarted','sobh':sobh,'shahr':shahr})
+        
+        '''calculate remaining time'''
+        remaininghour = maghrebtime.hour - now.hour
+        remainingminute = maghrebtime.minute - now.minute
+        if remainingminute < 0:
+            remaininghour -= 1
+            remainingminute = 60 + remainingminute
+        remainingtimestr = f'{remaininghour} Hours and {remainingminute} Minutes'
+        return render_template('home.html',data={'status':'remaining','maghreb':maghreb,'hour':remaininghour,'minute':remainingminute,'shahr':shahr})
+
+    else:
+        return 'An error occurred during connecting to "Aviny.com" API.'
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000, debug=True)
